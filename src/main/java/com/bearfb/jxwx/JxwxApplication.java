a443aea2f3213d5016cffd5e093e81bcde3da784
giraffecode9668 <ziminlin@126.com>
2019-08-13 16:25:18 +0800

    Initial commit

---
+++ src/main/java/com/bearfb/jxwx/JxwxApplication.java
@@ -0,0 +1,13 @@
+package com.bearfb.jxwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxwxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxwxApplication.class, args);
+    }
+
+}
