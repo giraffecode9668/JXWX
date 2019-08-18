e8652593f502ca9952b4cf169f1e2f0ed383b4ec
giraffecode9668 <ziminlin@126.com>
2019-08-18 11:34:46 +0800

    端口号更改为8081、历史上的今天首页路由

---
+++ src/main/java/com/bearfb/jxwx/modules/history/controller/HistoryHomeController.java
@@ -0,0 +1,26 @@
+package com.bearfb.jxwx.modules.history.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ *  历史上的今天
+ * </p>
+ *
+ * @author linzimin
+ * @date 2019/8/18 11:19
+ */
+@Controller
+@RequestMapping(value = {"/","/home","/today"})
+public class HistoryHomeController {
+
+    /**
+     * 历史上的今天·首页
+     * @return 返回"today/home"页面
+     */
+    @RequestMapping(value = {"","/","/home","home.html"})
+    public String historyPage(){
+        return "modules/today/home";
+    }
+}
